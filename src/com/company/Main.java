fbf07f7c041e55b2dac410eac559c6c19a46f6ab
chainley56 <cjhainley@gmail.com>
2017-11-10 06:59:25 -0500

    Really not sure how to do this

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
-
+import java.util.*;
+import java.io.*;
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
 	// write your code here
+        Scanner reader = new Scanner(new File("Imports.txt"));
+        int maxIndex = -1;
+        String values [] = new String[1000];
+        
+        while (reader.hasNext()){
+            maxIndex++;
+            values[maxIndex]=reader.nextLine();
+        }
     }
 }
