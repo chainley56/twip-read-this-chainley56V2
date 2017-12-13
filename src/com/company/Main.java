65fe41f8e6a60ef8dca2bcbd305cbb2ccf0dd740
ch983 <cjhainley@gmail.com>
2017-12-13 15:05:08 -0500

    t

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,26 @@ import java.util.*;
 import java.io.*;
 public class Main {
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args)throws IOException {
 	// write your code here
         Scanner reader = new Scanner(new File("Imports.txt"));
         int maxIndex = -1;
-        String values [] = new String[1000];
-        
+        String values [] = new String[26];
+
         while (reader.hasNext()){
             maxIndex++;
             values[maxIndex]=reader.nextLine();
         }
+        //System.out.println(values[0]);
+        //Print out avergage
+        //split
+        String grades [] = new String[1000];
+        for(int i =1; i <  values.length; i++)
+        {
+            String [] newA  = values[i].split("\t");
+            grades[i-1]= newA[2];
+
+        }
+        System.out.println(grades[0]);
     }
 }
