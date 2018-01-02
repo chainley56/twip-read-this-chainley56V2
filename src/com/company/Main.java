ac82d55e21def0146f23d4d1ea2b5263d571a7d2
cjhainley56 <cjhainley@gmail.com>
2018-01-01 22:15:52 -0500

    still dont know

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,26 +3,46 @@ import java.util.*;
 import java.io.*;
 public class Main {
 
-    public static void main(String[] args)throws IOException {
-	// write your code here
+    public static void main(String[] args) throws IOException
+    {
         Scanner reader = new Scanner(new File("Imports.txt"));
-        int maxIndex = -1;
-        String values [] = new String[26];
-
-        while (reader.hasNext()){
-            maxIndex++;
-            values[maxIndex]=reader.nextLine();
-        }
-        //System.out.println(values[0]);
-        //Print out avergage
-        //split
-        String grades [] = new String[1000];
-        for(int i =1; i <  values.length; i++)
+        int maxIndx = -1;
+        String values[]=new String[1000];
+        while(reader.hasNext())
         {
-            String [] newA  = values[i].split("\t");
-            grades[i-1]= newA[2];
-
+            maxIndx++;
+            values[maxIndx]=reader.nextLine();
         }
-        System.out.println(grades[0]);
-    }
-}
+        int avg=0;
+        int As =0;
+        int Bs= 0;
+        int Cs=0;
+        int Ds=0;
+        int Fs=0;
+        int j;
+        int letterGrade;
+        String Letters= "";
+        for (j = 0; j <= maxIndx; j++)
+        {
+            Scanner sc = new Scanner(values[j].substring(0,1) );
+            letterGrade = sc.next();
+            avg =+ letterGrade;
+            int absences = sc.nextInt();
+            if (letterGrade>89){
+                As++;
+            }
+            else if (letterGrade>79){
+                Bs++;
+            }
+            else if (letterGrade>69){
+                Cs++;
+            }
+            else if (letterGrade>59){
+                Ds++;
+            }
+            else{
+                Fs++;
+            }
+        }
+        System.out.println("The Class average is " + avg/j + "%");
+        System.out.println("There were "+As+" A's, "+Bs+"B's, "+Cs+"C's, "+Ds+"D's, and "+Fs+"F's.");}}
